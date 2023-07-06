591cdf794363a2a9c1d48a35658838c1b1a99824
Codingmasterfred <fredrickg777@gmail.com>
2023-07-06 07:51:51 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            PrepWork();
+        }
+        static void PrepWork()
+        {
+            Console.WriteLine("PrepWork");
+            int[] nums = new int[5];
+            for(int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+        }
+       
+    }
+}
