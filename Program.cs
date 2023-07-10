7d7da4058a487c70ff457aaef89bb739fe402984
Codingmasterfred <fredrickg777@gmail.com>
2023-07-10 07:37:36 -0700

    Updated

--- Program.cs
+++ Program.cs
@@ -4,58 +4,78 @@
     {
         static void Main(string[] args)
         {
-            try
-            {
-                StartSequence();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("An error occurred: " + ex.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Finally block executed.");
-            }
+            Console.WriteLine("Hello, World!");
+            StartSequence();
         }
-
         static void StartSequence()
         {
-            int[] MainArray = new int[6];
-            try
-                {
-                        Console.WriteLine("Enter a number greater than zero:");
-                    for (int i = 0; i < 6; i++)
-                    {
-                        int size = Convert.ToInt32(Console.ReadLine());
-
-                        if (size <= 0)
-                        {
-                            Console.WriteLine("The number must be greater than zero.");
-                        return;
-                        }
-
-                        if (size > 0)
-                    {
-                        Console.WriteLine("Enter another number.");
-                    }
-
-                        MainArray[i] = size;
-                        Console.WriteLine("Array created successfully with size: " + MainArray);
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Invalid input. Please enter a valid number.");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("An error occurred: " + ex.Message);
-                }
-                finally
-                {
-                    Console.WriteLine("Finally block executed.");
-                }
-          
+            int[] numberArray = new int[4]; // declaring array of four zeros
+            int[] populateResult = Populate(numberArray); // calling populate method
+            int sum = GetSum(populateResult);
+            Console.WriteLine(sum);
+            for (int i = 0; i < populateResult.Length; i++) //  loop through array and console.write elements
+            {
+                Console.Write(populateResult[i] + "");
+            }
+            Console.WriteLine();
+            GetProduct(populateResult, sum);
+            int product = GetProduct(populateResult, sum);
+            GetQuotient(product);
+        }
+        static int[] Populate(int[] array)
+        {
+            //loop through the array and prompt user for values
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Give me a number");
+                string userInput = Console.ReadLine();
+                int number = Int32.Parse(userInput);
+                array[i] = number;
+            }
+            return array;
+        }
+        static int GetSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            if (sum < 20)
+            {
+                throw new Exception("Number is too low");
+            }
+            return sum;
+        }
+        static int GetProduct(int[] array, int sum)
+        {
+            Console.WriteLine("Pick a random number 1 and {0}", array.Length);
+            string randomIndex = Console.ReadLine();
+            int randomIndexInt = Int32.Parse(randomIndex);
+            randomIndexInt = randomIndexInt - 1;
+            int randomNumber = array[randomIndexInt];
+            Console.WriteLine("random number is: {0}", randomNumber);
+            int product = randomNumber * sum;
+            Console.WriteLine("this is the product: {0}", product);
+            if (randomIndexInt < 0 || randomIndexInt >= array.Length)
+            {
+                throw new IndexOutOfRangeException("Index is out of range");
+            }
+            return product;
+        }
+        static decimal GetQuotient(int product)
+        {
+            // prompt the user for number to divide product by
+            Console.WriteLine("Give me a number to divide the product by");
+            string userInput = Console.ReadLine();
+            int divisor = Int32.Parse(userInput);
+            decimal quotient = product / divisor;
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            Console.WriteLine("This is the quotient: {0}", quotient);
+            return quotient;
         }
     }
 }
