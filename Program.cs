d6d4c2509ea54660b7f6d0e39cb4fb109109f3fd
Codingmasterfred <fredrickg777@gmail.com>
2023-07-10 07:34:56 -0700

    Update

--- Program.cs
+++ Program.cs
@@ -4,18 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            PrepWork();
-        }
-        static void PrepWork()
-        {
-            Console.WriteLine("PrepWork");
-            int[] nums = new int[5];
-            for(int i = 0; i < nums.Length; i++)
+            try
             {
-                nums[i] = Int32.Parse(Console.ReadLine());
+                StartSequence();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed.");
             }
         }
-       
+
+        static void StartSequence()
+        {
+            int[] MainArray = new int[6];
+            try
+                {
+                        Console.WriteLine("Enter a number greater than zero:");
+                    for (int i = 0; i < 6; i++)
+                    {
+                        int size = Convert.ToInt32(Console.ReadLine());
+
+                        if (size <= 0)
+                        {
+                            Console.WriteLine("The number must be greater than zero.");
+                        return;
+                        }
+
+                        if (size > 0)
+                    {
+                        Console.WriteLine("Enter another number.");
+                    }
+
+                        MainArray[i] = size;
+                        Console.WriteLine("Array created successfully with size: " + MainArray);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Finally block executed.");
+                }
+          
+        }
     }
 }
